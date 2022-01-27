2de2d919bd6aef34918ba38bac014dc68247ee60
sanket_ghormode <sanghormode2@gmail.com>
2022-01-27 13:00:07 +0530

    .gitignore fix

--- demo.js
+++
@@ -1,9 +0,0 @@
-let bcrypt = require('bcryptjs');
-let salt = bcrypt.genSaltSync(10);
-let hash = bcrypt.hashSync("Sanket@123", salt);
-console.log(hash);
-// Store hash in your password DB.
-
-// Load hash from your password DB.
-console.log(bcrypt.compareSync("S23", hash)); // true
-console.log(bcrypt.compareSync("not_bacon", hash)); // false  
