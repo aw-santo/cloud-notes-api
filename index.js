67cddf4a5c4adfb739fd14f1968aebcc209f2497
sanket_ghormode <sanghormode2@gmail.com>
2022-01-30 14:36:30 +0530

    cors added

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const connectToMongo = require('./db');
 
 connectToMongo();
+const cors = require('cors');
 
 const express = require('express');
 const app = express();
 const port = 5000;
 
 app.use(express.json());  // to receive data from the body
+app.use(cors());
 
 // Routes
 app.use('/api/auth', require('./routes/auth'));
